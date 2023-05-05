ece7b8c947f3a84c4446841001738d8fb2c80aa4
kioni kamau <kkamau@plu.edu>
2023-05-04 21:59:32 -0500

    now takes in paramter arguments and parses

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ import winnerEval
 #Error above will vanish when frontend combines with backend, and Math might be used, review later
 #if math is actually needed
 from flask_cors import CORS
+import json
 
 
 app = Flask(__name__)
@@ -16,10 +17,16 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route("/AI-Turn", methods=['GET'])
 def get_AI_move():
-    game_state = request.get_json(force=True)
+    game_state_imut = request.args
+    game_state = {}
+    for i in game_state_imut:
+        game_state[i] = json.loads(game_state_imut[i])
     return UseModel.model_nextmove(dict(game_state))
 
 @app.route("/Get-Winner", methods=['GET'])
 def get_winner_move():
-    game_state = request.get_json(force=True)
+    game_state_imut = request.args
+    game_state = {}
+    for i in game_state_imut:
+        game_state[i] = json.loads(game_state_imut[i])
     return str(winnerEval.get_winner(dict(game_state)))
